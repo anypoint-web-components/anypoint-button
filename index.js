612b99a0d52e6f98a91b9f1a31321d6d2b52f045
Pawel <jarrodek@gmail.com>
2020-05-06 11:16:18 -0700

    chore: adding base class to index exports

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export { AnypointButton } from './src/AnypointButton.js';
 export { AnypointIconButton } from './src/AnypointIconButton.js';
+export { AnypointButtonBase } from './src/AnypointButtonBase.js';
